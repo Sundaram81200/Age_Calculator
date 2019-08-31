f5f028b933440efc41dae046e7c7936c9cf66847
mahesh <mailtomahesh2000@gmail.com>
2019-09-01 01:42:37 +0530

    Age Calculator

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(primarySwatch: Colors.brown),
+    home: MyHomePage()));
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  double age = 0.0;
+  var selectedYear;
+
+  void _showPicker() {
+    showDatePicker(
+            context: context,
+            firstDate: DateTime(1900),
+            initialDate: DateTime(2018),
+            lastDate: DateTime.now())
+        .then((DateTime dt) {
+      selectedYear = dt.year;
+      calculateAge();
+    });
+  }
+
+  void calculateAge() {
+    setState(() {
+      age = (2018 - selectedYear).toDouble();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Age Calculator"),
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: Colors.redAccent,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                child: Text(
+                    selectedYear != null ? selectedYear.toString() : "    Year   ",textScaleFactor: 2.0,),
+                textColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                color: Colors.brown,
+                onPressed: _showPicker,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+              ),
+              Text(
+                "Your age is ${age.toStringAsFixed(0)}",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
