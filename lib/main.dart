055f6275b832350af4f6172f00626e476361cac3
mahesh <mailtomahesh81200@gmail.com>
2019-10-13 01:05:09 +0530

    readme

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    theme: ThemeData(primarySwatch: Colors.red),
+    theme: ThemeData(primarySwatch: Colors.green),
     home: MyHomePage()));
 
 class MyHomePage extends StatefulWidget {
